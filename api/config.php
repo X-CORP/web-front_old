ffb22955816e09c167d8caf1eee41af8d4b8e205
pgarcia-xymbi-corporation <patrick.garcia@xymbi.com>
2015-05-14 05:07:28 -0500

    original landing pre-kickstarter page

---
+++ api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'dGl1gu3KtkcNjePsn3vz3r2RL');  // CONSUMER_KEY_HERE
+    define('CONSUMER_SECRET', 'OYj6XJiUQiyB9WhAAVQkCylexXgQQBFF58vK7PHryOs6cwKxnk'); // CONSUMER_SECRET_HERE
+
+    // User Access Token
+    define('ACCESS_TOKEN', '736247215-uSMpcF6dN4ibHctlCv8sZV7XNysiovsvhOpR01rG'); // ACCESS_TOKEN_HERE
+    define('ACCESS_SECRET', 'eNQMGVHD6blSMMJzSqqP6FjwV7gLi7l1acgjjrTdlo6mo'); // ACCESS_SECRET_HERE
